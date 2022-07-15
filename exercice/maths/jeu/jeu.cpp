a6e1f1d3e602a9dcb93551dbf9ecc01da1673b2b
BuildTools <unconfigured@null.spigotmc.org>
2022-07-15 11:04:32 +0200

    amelioration jeu ! pas encore fini

--- exercice/maths/jeu/jeu.cpp
+++ exercice/maths/jeu/jeu.cpp
@@ -1,4 +1,6 @@
 #include <iostream>
+
+#include <time.h>
 #include <cstdlib>
 
 using namespace std;
@@ -8,13 +10,39 @@ using namespace std;
 
 int main(){
 
-    // cout << "Donnez moi le nombre ou vous voulez la somme de ses nombres : ";
+    cout << "Donnez moi le nombre ou vous voulez la somme de ses nombres : ";
+    int nomb(0);
+    int dem(0);
+    int test(0);
+    bool no(false);
+    srand(time(NULL));
+    nomb = rand()%500;
+    cin >> dem;
 
-    float nomb(0);
 
-    // cin >> nomb;
+    while(nomb != dem){
+        test++;
+    
 
-    nomb = rand();
-    cout << rand();
+        if(dem < nomb){
+            cout << "Trop petit essaye encore !" << endl;
+        }else{
+            cout << "Trop grand essaye encore !" << endl;
+        }
+
+        if(test == 10) {
+            no = true;
+            break;
+        }
+        cin >> dem;
+    }
+
+    if(no){
+        cout << "Dommage tu n'a pas trouve la bonne reponse, c'etait : " << nomb;
+        return 0;
+    }
+
+    cout << "Bien joue la bonne reponse etait bien " << nomb;
+
     return 0;
 }
